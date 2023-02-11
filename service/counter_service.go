2e3e2af3ba6c74f3a13689609919168b48ccfecd
13232160100 <101637168+1323216010@users.noreply.github.com>
2023-02-11 17:58:42 +0800

    回退

--- service/counter_service.go
+++ service/counter_service.go
@@ -22,12 +22,12 @@ type JsonResult struct {
 
 // IndexHandler 计数器接口
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := getIndex()
+	data, err := getIndex()
 	if err != nil {
 		fmt.Fprint(w, "内部错误")
 		return
 	}
-	fmt.Fprint(w, "data233")
+	fmt.Fprint(w, data)
 }
 
 // CounterHandler 计数器接口
