35fd0890233356b44589c5ac1784b71ade254936
13232160100 <101637168+1323216010@users.noreply.github.com>
2023-02-11 17:55:06 +0800

    测试

--- service/counter_service.go
+++ service/counter_service.go
@@ -22,7 +22,7 @@ type JsonResult struct {
 
 // IndexHandler 计数器接口
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := getIndex()
+	_, err := getIndex()
 	if err != nil {
 		fmt.Fprint(w, "内部错误")
 		return
